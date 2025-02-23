e7056daa9dbf096dc9bcc34ed01383a3a2b23649
fedecaruso1994 <federicocaruso.19@gmail.com>
2025-02-23 20:30:30 -0300

    Creando nodo y lista enlazada

--- desafioDevsys/DoublyLinkedList.h
+++ desafioDevsys/DoublyLinkedList.h
@@ -1,1 +1,18 @@
-#pragma once
+#include "Node.h"
+
+
+class DoublyLinkedList {
+
+private:
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+
+};
