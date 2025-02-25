bdcc2c99117e2b4b46655b0a092b6918f95d0e85
fedecaruso1994 <federicocaruso.19@gmail.com>
2025-02-25 09:21:41 -0300

    agregando metodos add y delete

--- desafioDevsys/DoublyLinkedList.h
+++ desafioDevsys/DoublyLinkedList.h
@@ -15,4 +15,58 @@ public:
         size = 0;
     }
 
+    int getSize() const {
+        
+        return size;
+    
+    }
+
+    void addElementStart(int value) {
+        Node* newNode = new Node(value);  // Creamos un nuevo nodo
+
+        if (!head) {  // Lista vacía
+            head = tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+
+        size++;  // Incrementamos el tamaño
+    }
+
+    void addElementEnd(int value) {
+        Node* newNode = new Node(value);  // Creamos un nuevo nodo
+
+        if (!tail) {  // Lista vacía
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+
+        size++;  // Incrementamos el tamaño
+    }
+
+    void deleteElementStart() {
+
+        if (!head) {
+            return;
+        }
+
+        head = head->next;
+        head->prev = nullptr;
+    }
+
+    void deleteElementEnd() {
+        if (tail == nullptr) {
+            return;
+        }
+
+        tail = tail->prev;
+        tail->next = nullptr;
+    }
 };
